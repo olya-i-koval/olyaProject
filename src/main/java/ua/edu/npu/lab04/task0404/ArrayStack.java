2a3c38da160457890a70cb6a8e71abc66c5a3534
koval-olya3 <63600652+koval-olya3@users.noreply.github.com>
2020-05-10 22:34:36 +0300

    Add task04 for lab 4

--- src/main/java/ua/edu/npu/lab04/task0404/ArrayStack.java
+++ src/main/java/ua/edu/npu/lab04/task0404/ArrayStack.java
@@ -1,22 +1,6 @@
 package ua.edu.npu.lab04.task0404;
 
 public class ArrayStack implements Stack{
-    public static void main(String[] args) {
-        ArrayStack stack = new ArrayStack(6);
-        stack.push(4);
-        stack.push(1);
-        stack.push(54);
-        stack.isEmpty();
-        stack.pop();
-        stack.size();
-
-        while (!stack.isEmpty()) {
-            int value = stack.pop();
-            System.out.print(value);
-            System.out.print(" ");
-        }
-        stack.clear();
-    }
 
     public int mSize;
     public int top;
--- src/main/java/ua/edu/npu/lab04/task0404/stack2/ArrayStack.java
+++ src/main/java/ua/edu/npu/lab04/task0404/stack2/ArrayStack.java
@@ -1,4 +1,37 @@
 package ua.edu.npu.lab04.task0404.stack2;
+import ua.edu.npu.lab04.task0404.Stack;
 
-public class ArrayStack {
+import java.util.ArrayList;
+
+public class ArrayStack implements Stack {
+
+    public static ArrayList<Integer> a = new ArrayList<>();
+
+    @Override
+    public void push(int data) {
+        a.add(0, data);
+
+    }
+
+    @Override
+    public int pop() {
+        a.remove(0);
+        return a.get(0);
+    }
+
+    @Override
+    public int size() {
+        return a.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return a.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        a.clear();
+    }
 }
+
