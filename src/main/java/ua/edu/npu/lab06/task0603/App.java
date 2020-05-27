c7b6befdcf4dee53af13fe411afb4daecd67eb89
koval-olya3 <63600652+koval-olya3@users.noreply.github.com>
2020-05-27 13:53:27 +0300

    Add task 3 for lab 6

--- src/main/java/ua/edu/npu/lab06/task0603/App.java
+++ src/main/java/ua/edu/npu/lab06/task0603/App.java
@@ -1,30 +1,26 @@
 package ua.edu.npu.lab06.task0603;
 
-public class App implements InterfaceApp {
-
+public class App implements AppInterface {
     public static void main(String[] args) {
         App app = new App();
-        app.method("1 кОзак з казок 1");
+        app.method("64 козак з казок 46");
     }
-
     @Override
     public String method(String line) {
-        String myLine = line.toLowerCase();
-        String reverse = new StringBuffer(myLine).reverse().toString();
+        String reverse = new StringBuffer(line).reverse().toString();
         System.out.println(reverse);
 
-        String str = myLine.replaceAll("[^а-я]", "");
-        System.out.println(str);
-
-
+        String myLine = line.toLowerCase();
         char[] lineArray = myLine.toCharArray();
-        for (int i =0; i<lineArray.length; i++) {
-            if( lineArray[i] ==lineArray[lineArray.length-1]) {
-                System.out.println("паліндром");
-            }
-            else break;
-        }
 
+        String newLine = myLine.replaceAll("[^а-я]", "");
+        System.out.println(newLine);
+
+        for (int i = 0; i < lineArray.length; i++) {
+            if (lineArray[i] == lineArray[lineArray.length-1]) {
+            }
+        }
+        System.out.println("паліндром");
         return null;
     }
 }
---
+++ src/main/java/ua/edu/npu/lab06/task0603/AppInterface.java
@@ -0,0 +1,5 @@
+package ua.edu.npu.lab06.task0603;
+
+public interface AppInterface {
+    String method (String line);
+}
